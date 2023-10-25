6d221191216f9bdd6b20aa635fe9850e6d40d3b8
@johnya0 <anoopjohny2000@gmail.com>
2023-10-25 19:00:43 +0200

    commit : uploading draw_shapes_text.py

--- draw_shapes_text.py
+++ draw_shapes_text.py
@@ -7,7 +7,17 @@ cv.imshow('Blank', blank)
 #img = cv.imread('Images/Cats.jpg')  # read the image
 #cv.imshow('Cat', img) # show the image
 
-blank[:] = 0,255,0
-cv.imshow('Green', blank)
+# blank[:] = 0,255,0   # b , g , r   ### this will be green
+# blank[:] = 0,0,255   # b , g , r   ### this will be red
+# blank[:] = 255,0,0   # b , g , r   ### this will be blue
 
+#blank[:] = 0,123,122   # b , g , r   ### this will be lime brown... using these u get different combos of colors
+
+# blank[200:300,300:400] = 0,123,122 ## set the size of the square
+
+cv.rectangle(blank,(0,0),(250,250),(0,255,0),thickness=2)  # get a green rectangle from the origin
+
+#cv.imshow('Green', blank)
+
+cv.imshow("rectangle",blank)
 cv.waitKey(0)
